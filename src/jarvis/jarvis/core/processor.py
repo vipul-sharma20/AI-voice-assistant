eecb17c12acdaab7cf706ccd59779fde213a5840
Vipul Sharma <vipul.sharma20@gmail.com>
2019-10-25 20:51:39 +0530

    fix: Code formatting

--- src/jarvis/jarvis/core/processor.py
+++ src/jarvis/jarvis/core/processor.py
@@ -42,36 +42,31 @@ from jarvis.core.console_manager import ConsoleManager
 class Processor:
     def __init__(self):
         self.input_engine = STTEngine(
-                                        pause_threshold=SPEECH_RECOGNITION['pause_threshold'],
-                                        energy_theshold=SPEECH_RECOGNITION['energy_threshold'],
-                                        ambient_duration=SPEECH_RECOGNITION['ambient_duration'],
-                                        dynamic_energy_threshold=SPEECH_RECOGNITION['dynamic_energy_threshold'],
-                                        sr=sr
-                                        ) if GENERAL_SETTINGS['user_voice_input'] else TTTEngine()
+            pause_threshold=SPEECH_RECOGNITION['pause_threshold'],
+            energy_theshold=SPEECH_RECOGNITION['energy_threshold'],
+            ambient_duration=SPEECH_RECOGNITION['ambient_duration'],
+            dynamic_energy_threshold=SPEECH_RECOGNITION['dynamic_energy_threshold'],
+            sr=sr) if GENERAL_SETTINGS['user_voice_input'] else TTTEngine()
 
-        self.console_manager = ConsoleManager(
-                                              log_settings=ROOT_LOG_CONF,
-                                             )
+        self.console_manager = ConsoleManager(log_settings=ROOT_LOG_CONF, )
         self.output_engine = TTSEngine(
-                                        console_manager=self.console_manager,
-                                        speech_response_enabled=GENERAL_SETTINGS['response_in_speech']
-                                       )
+            console_manager=self.console_manager,
+            speech_response_enabled=GENERAL_SETTINGS['response_in_speech'])
         self.response_creator = ResponseCreator()
 
         self.skill_analyzer = SkillAnalyzer(
-                                            weight_measure=TfidfVectorizer,
-                                            similarity_measure=cosine_similarity,
-                                            args=ANALYZER['args'],
-                                            skills_=SKILLS,
-                                            sensitivity=ANALYZER['sensitivity']
-                                            )
+            weight_measure=TfidfVectorizer,
+            similarity_measure=cosine_similarity,
+            args=ANALYZER['args'],
+            skills_=SKILLS,
+            sensitivity=ANALYZER['sensitivity'])
 
         self.skill_controller = SkillController(
-                                                settings_=GENERAL_SETTINGS,
-                                                input_engine=self.input_engine,
-                                                analyzer=self.skill_analyzer,
-                                                control_skills=CONTROL_SKILLS,
-                                                )
+            settings_=GENERAL_SETTINGS,
+            input_engine=self.input_engine,
+            analyzer=self.skill_analyzer,
+            control_skills=CONTROL_SKILLS,
+        )
 
     def run(self):
         start_up()
@@ -81,12 +76,14 @@ class Processor:
                 self._process()
 
     def _process(self):
-            self.skill_controller.get_transcript()
-            self.skill_controller.get_skills()
-            if self.skill_controller.to_execute:
-                response = self.response_creator.create_positive_response(self.skill_controller.latest_voice_transcript)
-            else:
-                response = self.response_creator.create_negative_response(self.skill_controller.latest_voice_transcript)
+        self.skill_controller.get_transcript()
+        self.skill_controller.get_skills()
+        if self.skill_controller.to_execute:
+            response = self.response_creator.create_positive_response(
+                self.skill_controller.latest_voice_transcript)
+        else:
+            response = self.response_creator.create_negative_response(
+                self.skill_controller.latest_voice_transcript)
 
-            self.output_engine.assistant_response(response)
-            self.skill_controller.execute()
+        self.output_engine.assistant_response(response)
+        self.skill_controller.execute()
